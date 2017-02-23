88f956b46846581481a01a450979802a18922bd3
BrunoBFerreira <bruno.b.ferreira@tecnico.ulisboa.pt>
2017-02-23 14:08:11 +0000

    Updated copyAddon

--- copyAddon.bat
+++ copyAddon.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-IF EXIST "D:\GRAPHISOFT\ArchiCAD 18\Add-Ons\RosettaArchiCAD.apx"(del "D:\GRAPHISOFT\ArchiCAD 18\Add-Ons\RosettaArchiCAD.apx") ELSE()
-copy "D:\GRAPHISOFT\API Development Kit 18.3006\Examples\Geometry_Test - Copy\x64\RosettaArchiCAD.apx" "D:\GRAPHISOFT\ArchiCAD 18\Add-Ons"
+IF EXIST "C:\Program Files\GRAPHISOFT\ArchiCAD 18\Add-Ons\RosettaArchiCAD.apx"(del "C:\Program Files\GRAPHISOFT\ArchiCAD 18\Add-Ons\RosettaArchiCAD.apx") ELSE()
+copy "C:\Program Files\GRAPHISOFT\API Development Kit 18.3006\Examples\Geometry_Test - Copy\x64\RosettaArchiCAD.apx" "C:\Program Files\GRAPHISOFT\ArchiCAD 18\Add-Ons"
